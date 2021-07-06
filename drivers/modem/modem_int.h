a50b0bec4395f50155cb827422f5bdc1cf123531
Bee-Vision <72527524+Bee-Vision@users.noreply.github.com>
2021-07-06 14:22:11 -0700

    Update modem_int.h
    Added Changes for Modem Imei

--- drivers/modem/modem_int.h
+++ drivers/modem/modem_int.h
@@ -7,10 +7,33 @@
 
 #include <time.h>
 
+/* Default lengths */
+#define MODEM_MANUFACTURER_LENGTH   10
+#define MODEM_MODEL_LENGTH          16
+#define MODEM_REVISION_LENGTH       64
+#define MODEM_IMEI_LENGTH           16
+#define MODEM_IMSI_LENGTH           16
+#define MODEM_ICCID_LENGTH          32
+#define MODEM_INFO_NETWORK_MODE_MAX_SIZE 12
+
+struct modem_info {
+    char manufacturer[MODEM_MANUFACTURER_LENGTH];
+    char model[MODEM_MODEL_LENGTH];
+    char revision[MODEM_REVISION_LENGTH];
+    char imei[MODEM_IMEI_LENGTH];
+#if defined(CONFIG_MODEM_SIM_NUMBERS)
+    char imsi[MODEM_IMSI_LENGTH];
+    char iccid[MODEM_ICCID_LENGTH];
+#endif /* #if defined(CONFIG_MODEM_SIM_NUMBERS) */
+};
+
 int modem_get_date(struct tm *tm);
+
+int modem_get_info(struct modem_info *info);
 
 void modem_reset(void);
 
 void modem_off(void);
+
 
 #endif /* #ifndef MODEM_INT_H */
